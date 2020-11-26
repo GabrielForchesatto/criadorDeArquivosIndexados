4fa6792bedf3485d90b1e965313ff21a84dd7f67
Gabriel Forchesatto <gabrielvalendorf@gmail.com>
2020-11-26 10:33:02 -0300

    stemming implementado

--- main.py
+++ main.py
@@ -2,9 +2,43 @@
 import os
 import nltk
 from nltk.stem import RSLPStemmer
+nltk.download('rslp')
 
-nltk.download('stopwords')
-stopwords = nltk.corpus.stopwords.words('portuguese')
+def Stemming(frase):
+    nucleo = RSLPStemmer()
+    fraseNucleo = []
+    for palavra in frase:
+        fraseNucleo.append(nucleo.stem(palavra.lower()))
+    return fraseNucleo
+
+
+
+stopwords = ['de', 'a', 'o', 'que', 'e', 'é', 'do', 'da', 'em', 'um', 'para', 'com', 'não', 'uma', 'os', 'no', 'se',
+             'na', 'por', 'mais', 'as', 'dos', 'como', 'mas', 'ao', 'ele', 'das', 'à', 'seu', 'sua', 'ou', 'quando',
+             'muito', 'nos', 'já', 'eu', 'também', 'só', 'pelo', 'pela', 'até', 'isso', 'ela', 'entre', 'depois', 'sem',
+             'mesmo', 'aos', 'seus', 'quem', 'nas', 'me', 'esse', 'eles', 'você', 'essa', 'num', 'nem', 'suas', 'meu',
+             'às', 'minha', 'numa', 'pelos', 'elas', 'qual', 'nós', 'lhe', 'deles', 'essas', 'esses', 'pelas', 'este',
+             'dele', 'tu', 'te', 'vocês', 'vos', 'lhes', 'meus', 'minhas', 'teu', 'tua', 'teus', 'tuas', 'nosso',
+             'nossa', 'nossos', 'nossas', 'dela', 'delas', 'esta', 'estes', 'estas', 'aquele', 'aquela', 'aqueles',
+             'aquelas', 'isto', 'aquilo', 'estou', 'está', 'estamos', 'estão', 'estive', 'esteve', 'estivemos',
+             'estiveram', 'estava', 'estávamos', 'estavam', 'estivera', 'estivéramos', 'esteja', 'estejamos', 'estejam',
+             'estivesse', 'estivéssemos', 'estivessem', 'estiver', 'estivermos', 'estiverem', 'hei', 'há', 'havemos',
+             'hão', 'houve', 'houvemos', 'houveram', 'houvera', 'houvéramos', 'haja', 'hajamos', 'hajam', 'houvesse',
+             'houvéssemos', 'houvessem', 'houver', 'houvermos', 'houverem', 'houverei', 'houverá', 'houveremos',
+             'houverão', 'houveria', 'houveríamos', 'houveriam', 'sou', 'somos', 'são', 'era', 'éramos', 'eram',
+             'fui', 'foi', 'fomos', 'foram', 'fora', 'fôramos', 'seja', 'sejamos', 'sejam', 'fosse', 'fôssemos',
+             'fossem', 'for', 'formos', 'forem', 'serei', 'será', 'seremos', 'serão', 'seria', 'seríamos', 'seriam',
+             'tenho', 'tem', 'temos', 'tém', 'tinha', 'tínhamos', 'tinham', 'tive', 'teve', 'tivemos', 'tiveram',
+             'tivera', 'tivéramos', 'tenha', 'tenhamos', 'tenham', 'tivesse', 'tivéssemos', 'tivessem', 'tiver',
+             'tivermos', 'tiverem', 'terei', 'terá', 'teremos', 'terão', 'teria', 'teríamos', 'teriam']
+
+def removerPontuacao(frase, paraRemover):
+    fraseLimpa = frase
+    for x in paraRemover:
+        fraseLimpa = fraseLimpa.replace(x, '')
+    return fraseLimpa
+
+
 
 novoDoc = input(str("Criar novo documento [S/N]? ")).lower()
 
@@ -17,10 +51,10 @@ if novoDoc == "s":
 
     conteudo = input(str(u'Digite o conteúdo do arquivo: ')).lower()
 
+    conteudo = removerPontuacao(conteudo, ".,:!?")
+
     arquivo.write(conteudo)  # escreve o conteúdo
 
-cont = 0
-fraseS = []
 indexados = {}
 
 for _, _, arquivo in os.walk('C:/Users/Usuario/OneDrive/Documentos/IMED/Estrutura de Dados/g2/docs/'):
@@ -46,7 +80,11 @@ for _, _, arquivo in os.walk('C:/Users/Usuario/OneDrive/Documentos/IMED/Estrutur
                 if frase[i] in stopwords:
                     frase.remove(frase[i])
 
+            frase = Stemming(frase)
+            print(frase)
 
-        print(frase) #não mexer, frase sem stop word
+
+
+
 
 
